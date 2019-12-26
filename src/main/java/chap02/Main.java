2df87a39f6af3b7e54da4bbe26e35987b5771791
성승익 <seunglk.seong>
2019-12-26 17:42:30 +0900

    Learn Dependency Injection

---
+++ src/main/java/chap02/Main.java
@@ -0,0 +1,17 @@
+package chap02;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// 1. 설정 정보를 이용해서 Bean객체를 생성한다.
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
+		// 2. Bean 객체를 제공한다.
+		Greeter g = ctx.getBean("greeter", Greeter.class);
+		String msg = g.greet("스프링");
+		System.out.println(msg);
+		ctx.close();
+	}
+
+}
