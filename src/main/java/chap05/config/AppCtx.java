139136034afdbe3abd608fc97236651e68f2608a
성승익 <seunglk.seong>
2019-12-31 17:46:28 +0900

    Learn component scan

---
+++ src/main/java/chap05/config/AppCtx.java
@@ -0,0 +1,37 @@
+package chap05.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import chap04.MemberPrinter;
+import chap04.MemberSummaryPrinter;
+import chap04.VersionPrinter;
+
+@Configuration
+@ComponentScan(basePackages = {"chap05"}, excludeFilters = @Filter(type = FilterType.REGEX, pattern = "chap05\\..*Dao"))
+public class AppCtx {
+	
+	
+	@Bean
+	@Qualifier("printer")
+	public MemberPrinter memberPrinter1() {
+		return new MemberPrinter();
+	}
+	@Bean
+	@Qualifier("summaryPrinter")
+	public MemberPrinter memberPrinter2() {
+		return new MemberSummaryPrinter();
+	}
+	
+	@Bean
+	public VersionPrinter versionPrinter() {
+		VersionPrinter versionPrinter = new VersionPrinter();
+		versionPrinter.setMajorVersion(5);
+		versionPrinter.setMinorVersion(0);
+		return versionPrinter;
+	}
+}
