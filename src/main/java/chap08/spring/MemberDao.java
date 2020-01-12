cde1b497a58f30a00fd559174d3a35c4891c5075
rotcafer <rotcafer@gmail.com>
2020-01-12 23:24:58 +0900

    Learn dbconnection

--- src/main/java/chap08/spring/MemberDao.java
+++ src/main/java/chap08/spring/MemberDao.java
@@ -1,10 +1,14 @@
 package chap08.spring;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class MemberDao {
 	private JdbcTemplate jdbcTemplate;
@@ -13,7 +17,22 @@ public class MemberDao {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	public Member selectByEmail(String email) {
-		return null;
+		List<Member> results = jdbcTemplate.query("select * from MEMBER where EMAIL = ?", new RowMapper<Member>() {
+
+			@Override
+			public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+				// TODO Auto-generated method stub
+				Member member = new Member(
+						rs.getString("EMAIL"),
+						rs.getString("PASSWORD"),
+						rs.getString("NAME"),
+						rs.getTimestamp("REGDATE").toLocalDateTime());
+				member.setId(rs.getLong("ID"));
+				return member;
+			}
+		}, email);
+		
+		return results.isEmpty() ? null : results.get(0);
 	}
 	
 	public void insert(Member member) {
