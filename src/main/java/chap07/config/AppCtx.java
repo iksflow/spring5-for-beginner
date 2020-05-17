2b3de6b4409ef29b3b962e21617c218974d7f906
rotcafer <rotcafer@gmail.com>
2020-05-17 23:46:13 +0900

    Aspect 오류수정

--- src/main/java/chap07/config/AppCtx.java
+++ src/main/java/chap07/config/AppCtx.java
@@ -1,4 +1,4 @@
-package chap07.config;
+   package chap07.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/chap07/main/MainAspect.java
+++ src/main/java/chap07/main/MainAspect.java
@@ -2,7 +2,7 @@ package chap07.main;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import chap07.RecCalculator;
+import chap07.Calculator;
 import chap07.config.AppCtx;
 
 public class MainAspect {
@@ -10,7 +10,7 @@ public class MainAspect {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 //		Calculator cal = ctx.getBean("calculator", Calculator.class);
-		RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
+		Calculator cal = ctx.getBean("calculator", Calculator.class);
 		long fiveFact = cal.factorial(5);
 		System.out.println("cal.factorial(5) = " + fiveFact);
 		System.out.println(cal.getClass().getName());
