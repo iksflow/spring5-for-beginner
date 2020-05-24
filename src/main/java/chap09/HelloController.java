00b22cfb2356f0cfff815c41d7b5c84bc6690031
rotcafer <rotcafer@gmail.com>
2020-05-24 22:24:14 +0900

    Request 매핑 사용법으로 변환해보기

--- src/main/java/chap09/HelloController.java
+++ src/main/java/chap09/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
 	
 	@GetMapping("/hello")
+	// @RequestMapping
 	public String hello(Model model,
 			@RequestParam(value = "name", required = false) String name) {
 		model.addAttribute("greeting", "안녕하세요" + name);
